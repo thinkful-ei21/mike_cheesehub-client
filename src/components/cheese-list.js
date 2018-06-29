1cdbcd60a1553a495821536a2f503ffac879e8f5
Michael Le-Reiver <m.lereiver@gmail.com>
2018-06-29 11:19:35 -0700

    created api endpoint, created react component, rendered react component

---
+++ src/components/cheese-list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function CheeseList(props){
+
+ const cheeses = props.cheeses.map((cheese, index) => (
+    <li key={index}>
+      {cheese}
+    </li>
+  ));
+
+  return (
+  <ul className='cheese-list'>
+    {cheeses}
+  </ul>
+  );
+}
+  
